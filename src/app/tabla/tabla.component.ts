994ff552fe78fc40ea408659624e6b5f03476d27
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-05-23 20:15:37 -0500

    commit

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -6,24 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabla.component.css']
 })
 export class TablaComponent {
-  nuevaTarea: string | null = null;
-  tareas: string[] = [];
+  tareas: string[] = ["Comprar víveres", "Lavar el auto", "Hacer ejercicio", "Estudiar para el examen", "Llamar al médico"];
 
-  onInputChange(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    this.nuevaTarea = inputElement.value;
-  }
-
-  agregarTarea(event: Event) {
-    event.preventDefault(); 
-
-    if (this.nuevaTarea) {
-      this.tareas.push(this.nuevaTarea);
-      this.nuevaTarea = '';
-    }
-  }
-
-  eliminarTarea(index: number) {
-    this.tareas.splice(index, 1);
-  }
 }
